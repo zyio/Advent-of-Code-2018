6d63a030f123eb6d460e039a7301d4a213dd668e
Sam Norbury <sam@zy.io>
2018-12-03 21:35:47 +0000

    Zip angers non ruby users

--- 2/2b.rb
+++ 2/2b.rb
@@ -1,18 +1,14 @@
 input=File.read('input.txt').split("\n")
 
-twocount=0
-threecount=0
-input.each do |line|
-  x=0
-  y=0
-  line.split('').uniq.each do |letter|
-    if line.split('').count(letter) == 3 and x < 1
-      threecount+=1
-      x+=1
-    elsif line.split('').count(letter) == 2 and y < 1
-      twocount+=1
-      y+=1
+input.each do |xline|
+  input.each do |yline|
+    if xline.split('').zip(yline.split('')).count {|i,j| i==j} == 25
+      out = []
+      xline.split('').zip(yline.split('')).count do |i,j|
+        out << i if i ==j        
+      end
+      p out.join
+      exit
     end
-  end
+  end   
 end
-p twocount * threecount
